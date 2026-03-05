ae72e48e4a594f03ea980a0778d1cbfb2738d2a9
manhleduy <mle529189@gmail.com>
2026-03-05 11:48:14 +0700

    second ready for vercel

--- src/components/LanguageSkills.tsx
+++ src/components/LanguageSkills.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import {motion} from 'framer-motion'
 import { languageSkills } from '@/data/skill'
 const LanguageSkills = () => {
--- src/components/SVG/Cloudinary.tsx
+++ src/components/SVG/Cloudinary.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 const SVGComponent = (props:any) => (
   <svg
     width="256px"
